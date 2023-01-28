216f41680611536ad5366f844aa42972e3b97d5f
Alejandro Quiñones <alejandro.quinones@outlook.es>
2023-01-28 19:06:46 -0300

    fourth

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 const message = require('./src/helper/message')
 const indexRoutes = require('./src/routes/indexRoutes')
-const PORT = proces.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
